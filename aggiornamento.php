e3d48f6693a271fc904a4ac90e98628ceaef611a
mamogmx <marco.carbone@gmx.com>
2014-04-03 15:35:21 +0200

    Added "system update" through PHP CONSTANT "UPDATE_SW"

---
+++ aggiornamento.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="/css/styles.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="/css/tabella_v.css" media="screen">
+</head>
+<body style="background-color:#FFFFFF">
+<?php
+$menu=0;
+include ("./inc/inc.page_header.php");
+?>
+
+
+<TABLE cellSpacing="0" cellPadding="0" width="100%" border="0">
+  <TBODY>
+  
+  <TR>
+	<td style="height:400px;text-align: center;color: red;font-size: 18px;font-weight: bold;">
+		
+            Il sistema &egrave; in fase di Aggiornamento.
+		
+	</td>
+	
+  </TR>
+  <TR><TD style="text-align:right; background-color:#728bb8; border-bottom:6px solid #415578; padding:6 6 1 6px" colspan="5"><a href="http://www.gisweb.it" target="_blank"><img src="images/logoblu.png" border="0"></a></td></TR>
+  </TBODY> 
+</TABLE>
+<!--<div style="background-image:url(images/sfondo.png); background-repeat:repeat-x; width:100%; height:8"></div>-->
+</body>
+</html>
--- login.php
+++ login.php
@@ -30,7 +30,10 @@
 	$dbconn=new sql_db(DB_HOST.":".DB_PORT,DB_USER,DB_PWD,DB_NAME, false);
 	if(!$dbconn->db_connect_id)  die( "Impossibile connettersi al database");
 	//Se sto validando l'utente includo la validazione, se va male esco altrimenti continuo a caricare la pagina stessa
-	
+	if ((defined('UPDATE_SW') && UPDATE_SW==1)){
+            require_once "aggiornamento.php";
+            exit;
+        }
 	if(isset($_POST['entra'])){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
