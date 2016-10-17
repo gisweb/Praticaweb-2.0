94860549fe19bb3d3d8e7b10366ade663e2a40e2
mamogmx <marco.carbone@gmx.com>
2016-10-17 16:13:53 +0200

    bug fix

--- services/xSearch.php
+++ services/xSearch.php
@@ -113,7 +113,7 @@ switch($action){
                 break;
             default:
                 $queryName="default";
-                $order = "ORDER BY coalesce(coalesce(data_prot,data_presentazione),'01/01/1970'::date) DESC";
+                $order = "ORDER BY coalesce(coalesce(data_prot,data_presentazione),'01/01/1970'::date)";
                 break;
         }
         foreach($data as $key=>$value){
