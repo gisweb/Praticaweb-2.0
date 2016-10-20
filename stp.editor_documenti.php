ca8cf08af5270c7785095634d13425e26d523da8
mamogmx <marco.carbone@gmx.com>
2016-10-20 15:00:01 +0200

    tinymce complete toolbar

--- stp.editor_documenti.php
+++ stp.editor_documenti.php
@@ -76,31 +76,50 @@ $testo="<html><head></head><body>$testo</body></html>";
 		//ed=new tinymce.Editor('elm1',{
 			// Location of TinyMCE script
 			script_url : '/js/tinymce/tinymce.js',
-
-			// General options
-			/*theme : "advanced",
-			plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist,example",
 			language : 'it',
-			// Theme options
-			theme_advanced_buttons1 : "save,cancel,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-			theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-			theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-			theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
-			theme_advanced_toolbar_location : "top",
-			theme_advanced_toolbar_align : "left",
-			theme_advanced_statusbar_location : "bottom",
-			theme_advanced_resizing : true,
-
-			// Example content CSS (should be your site CSS)
-			content_css : "css/content.css,css/stp.standard.css",
-
-			// Drop lists for link/image/media/template dialogs
-			template_external_list_url : "lists/template_list.js",
-			external_link_list_url : "lists/link_list.js",
-			external_image_list_url : "lists/image_list.js",
-			media_external_list_url : "lists/media_list.js",
-			theme_advanced_font_sizes : "3pt,4pt,5pt,6pt,7pt,8pt,9pt,10pt,11pt,12pt,13pt,14pt,15pt,16pt,17pt,18pt,19pt,20pt",
-			*/
+			plugins: [
+			"advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
+			"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+			"table contextmenu directionality emoticons template textcolor paste fullpage textcolor colorpicker textpattern"
+		  ],
+		
+		  toolbar1: "newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
+		  toolbar2: "cut copy paste | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | insertdatetime preview | forecolor backcolor",
+		  toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft",
+		
+		  menubar: false,
+		  toolbar_items_size: 'small',
+		
+		  style_formats: [{
+			title: 'Bold text',
+			inline: 'b'
+		  }, {
+			title: 'Red text',
+			inline: 'span',
+			styles: {
+			  color: '#ff0000'
+			}
+		  }, {
+			title: 'Red header',
+			block: 'h1',
+			styles: {
+			  color: '#ff0000'
+			}
+		  }, {
+			title: 'Example 1',
+			inline: 'span',
+			classes: 'example1'
+		  }, {
+			title: 'Example 2',
+			inline: 'span',
+			classes: 'example2'
+		  }, {
+			title: 'Table styles'
+		  }, {
+			title: 'Table row 1',
+			selector: 'tr',
+			classes: 'tablerow1'
+		  }],
 			save_onsavecallback : 'saveData',
 			save_oncancelcallback : function(v){
 				window.blur();
