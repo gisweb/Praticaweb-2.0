c562a7222b78e54bffdcedbc2cad822c19a09588
Marco Carbone <marco.carbone@gmx.com>
2016-11-24 06:54:01 +0100

    Update app.utils.class.php

--- lib/app.utils.class.php
+++ lib/app.utils.class.php
@@ -2,10 +2,8 @@
 use Doctrine\Common\ClassLoader;
 require_once APPS_DIR.'plugins/Doctrine/Common/ClassLoader.php';
 require_once LIB."utils.class.php";
-
-
-
-class generalAppUtils {
+require_once APPS_DIR.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'app.utils.class.php';
+class appUtils extends generalAppUtils {
    static function getDB(){
 		$classLoader = new ClassLoader('Doctrine', APPS_DIR.'plugins/');
 		$classLoader->register();
