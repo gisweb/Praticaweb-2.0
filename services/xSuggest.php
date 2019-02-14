9f51845cf5eee5dd52a6ff48ab7abbdbbbb5468d
Marco Carbone <marco.carbone@gmx.com>
2019-02-14 10:35:24 +0100

    Update xSuggest.php
    aggiornato a produzione

--- services/xSuggest.php
+++ services/xSuggest.php
@@ -9,78 +9,78 @@ $field=$_REQUEST['field'];
 $value=($_REQUEST['term'])?(addslashes($_REQUEST['term'])):(addslashes($_REQUEST['q']));
 $query=0;
 switch($field) {
-    case 'tavola':
-        $sql="SELECT nome_tavola as id,coalesce(descrizione,nome_tavola) as opzione FROM vincoli.tavola WHERE nome_vincolo='$value' or nome_vincolo is null order by ordine,2;";
-        if($db->sql_query($sql)){
-            $result=Array(Array("id"=>null,"opzione"=>'Seleziona =====>'));
-            $res=$db->sql_fetchrowset();//print_array($res);
+	case 'tavola':
+		$sql="SELECT nome_tavola as id,coalesce(descrizione,nome_tavola) as opzione FROM vincoli.tavola WHERE nome_vincolo='$value' or nome_vincolo is null order by ordine,2;";
+		if($db->sql_query($sql)){
+			$result=Array(Array("id"=>null,"opzione"=>'Seleziona =====>'));
+			$res=$db->sql_fetchrowset();//print_array($res);
             for($i=0;$i<count($res);$i++){
-                $result[]=Array("id"=>$res[$i]["id"],"opzione"=>$res[$i]["opzione"]);
-            }
-            $exec=1;
-        }
-        else
+				$result[]=Array("id"=>$res[$i]["id"],"opzione"=>$res[$i]["opzione"]);
+			}
+			$exec=1;
+		}
+		else
             $result[]=Array(
                 "id"=>'',
                 "value"=>'',
                 "label"=>"Si è verificato un errore nell' esecuzione dell'interrogazione $sql"
-
+                
             );
-        break;
-    case 'zona':
-        $vincolo=addslashes($_REQUEST['vincolo']);
+		break;
+	case 'zona':
+		$vincolo=addslashes($_REQUEST['vincolo']);
 
-        $sql="SELECT nome_zona as id,coalesce(descrizione,nome_zona) as opzione FROM vincoli.zona WHERE nome_tavola='$value' and nome_vincolo='$vincolo' order by ordine,2;";
-        if($db->sql_query($sql)){
-            $res=$db->sql_fetchrowset();//print_array($res);
-            $result=Array(Array("id"=>null,"opzione"=>'Seleziona =====>'));
+		$sql="SELECT nome_zona as id,coalesce(descrizione,nome_zona) as opzione FROM vincoli.zona WHERE nome_tavola='$value' and nome_vincolo='$vincolo' order by ordine,2;";
+		if($db->sql_query($sql)){
+			$res=$db->sql_fetchrowset();//print_array($res);
+			$result=Array(Array("id"=>null,"opzione"=>'Seleziona =====>'));
             for($i=0;$i<count($res);$i++){
-                $result[]=Array("id"=>$res[$i]["id"],"opzione"=>$res[$i]["opzione"]);
-            }
-            $exec=1;
-        }
-        else
+				$result[]=Array("id"=>$res[$i]["id"],"opzione"=>$res[$i]["opzione"]);
+			}
+			$exec=1;
+		}
+		else
             $result[]=Array(
                 "id"=>'',
                 "value"=>'',
                 "label"=>"Si è verificato un errore nell' esecuzione dell'interrogazione $sql"
-
+                
             );
-        break;
-    case 'motivo':
-        $motivo=addslashes($_REQUEST['motivo']);
-        switch($motivo){
-            case "2":
-                $sql="select * from admin.elenco_istruttori_tecnici where id=(SELECT resp_it FROM pe.avvioproc where pratica=); ";
-                break;
-            default:
-                $sql="SELECT * FROM admin.elenco_istruttori;";
-
-                break;
-        }
-        if($db->sql_query($sql)){
-            $res=$db->sql_fetchrowset();//print_array($res);
-            $result=Array(Array("id"=>null,"opzione"=>'Seleziona =====>'));
+		break;
+	case 'motivo':
+		$motivo=addslashes($_REQUEST['motivo']);
+		switch($motivo){
+			case "2":
+				$sql="select * from admin.elenco_istruttori_tecnici where id=(SELECT resp_it FROM pe.avvioproc where pratica=); ";
+				break;
+			default:
+				$sql="SELECT * FROM admin.elenco_istruttori;";
+				
+				break;
+		}
+		if($db->sql_query($sql)){
+			$res=$db->sql_fetchrowset();//print_array($res);
+			$result=Array(Array("id"=>null,"opzione"=>'Seleziona =====>'));
             for($i=0;$i<count($res);$i++){
-                $result[]=Array("id"=>$res[$i]["id"],"opzione"=>$res[$i]["opzione"]);
-            }
-            $exec=1;
-        }
-        else
+				$result[]=Array("id"=>$res[$i]["id"],"opzione"=>$res[$i]["opzione"]);
+			}
+			$exec=1;
+		}
+		else
             $result[]=Array(
                 "id"=>'',
                 "value"=>'',
                 "label"=>"Si è verificato un errore nell' esecuzione dell'interrogazione $sql"
-
+                
             );
-        break;
+		break;
     case 'codfis':
         require_once("../calcolacodicefiscale.php");
-        $cognome=$_REQUEST['cognome'];
-        $nome=$_REQUEST['nome'];
-        $sesso=$_REQUEST['sesso'];
-        $comune=addslashes($_REQUEST['comunato']);
-        $datanascita=$_REQUEST['datanato'];
+		$cognome=$_REQUEST['cognome'];
+		$nome=$_REQUEST['nome'];
+		$sesso=$_REQUEST['sesso'];
+		$comune=addslashes($_REQUEST['comunato']);
+		$datanascita=$_REQUEST['datanato'];
         $sql="SELECT codice FROM pe.e_comuni WHERE nome ilike '$comune' order by nome";
         if($db->sql_query($sql)){
             //$comune=$db->sql_fetchfield('codice');
@@ -93,7 +93,7 @@ switch($field) {
                 while (list ($key, $val) = each ($r->errori)) {
                     $errors.= ($key+1)." - $val;\n";
                 }
-            }
+            } 
             else{
                 $codfis= $r->codicefiscale;
             }
@@ -112,7 +112,7 @@ switch($field) {
             'citta'=>Array('cap'=>'cap','sigla_prov'=>'prov')
         );
         if($db->sql_query($sql)){
-
+            
             $res=$db->sql_fetchrowset();//print_array($res);
             for($i=0;$i<count($res);$i++){
                 $r=Array();
@@ -123,7 +123,7 @@ switch($field) {
                     "label"=>$res[$i]["nome"]." (".$res[$i]["sigla_prov"].") - ".$res[$i]["cap"],
                     "child"=>$r
                 );
-
+                
             }
             $exec=1;
         }
@@ -132,7 +132,7 @@ switch($field) {
                 "id"=>'',
                 "value"=>'',
                 "label"=>"Si è verificato un errore nell' esecuzione dell'interrogazione"
-
+                
             );
         break;
     case "sezione":
@@ -163,7 +163,7 @@ switch($field) {
 
     case "foglio":
         $sezione=($_REQUEST["sezione"])?($_REQUEST["sezione"]):('%');
-        $filtroComune = ($_REQUEST["cod_belfiore"])?(" AND comune = '".$_REQUEST["cod_belfiore"]."'"):("");
+	$filtroComune = ($_REQUEST["cod_belfiore"])?(" AND comune = '".$_REQUEST["cod_belfiore"]."'"):("");
         $sql="SELECT DISTINCT foglio as valore,length(foglio) FROM nct.particelle WHERE foglio ilike '%$value%' AND (sezione ilike '$sezione' or sezione is null) $filtroComune order by 2,1";
         if($db->sql_query($sql)){
             $res=$db->sql_fetchrowset();
@@ -173,23 +173,23 @@ switch($field) {
                     "value"=>$res[$i]["valore"],
                     "label"=>$res[$i]["valore"]
                 );
-
+                
             }
             $exec=1;
         }
-        else
-            $result[]=Array(
-                "id"=>'',
-                "value"=>'',
-                "label"=>"Si è verificato un errore nell' esecuzione dell'interrogazione;",
-                "query"=>$sql,
-                "field"=>$field
-            );
+		else
+        $result[]=Array(
+            "id"=>'',
+            "value"=>'',
+            "label"=>"Si è verificato un errore nell' esecuzione dell'interrogazione;",
+			"query"=>$sql,
+			"field"=>$field
+        );
         break;
     case 'mappale':
         $fg=(isset($_REQUEST['foglio']))?(addslashes($_REQUEST['foglio'])):('%');
         $sezione=($_REQUEST["sezione"])?($_REQUEST["sezione"]):('%');
-        $filtroComune = ($_REQUEST["cod_belfiore"])?(" AND comune = '".$_REQUEST["cod_belfiore"]."'"):("");
+	$filtroComune = ($_REQUEST["cod_belfiore"])?(" AND comune = '".$_REQUEST["cod_belfiore"]."'"):("");
         $sql="SELECT DISTINCT mappale as valore,CASE WHEN (regexp_replace(mappale,'([^0-9]+)','','g'))='' THEN 0 ELSE regexp_replace(mappale,'([^0-9]+)','','g')::integer end FROM nct.particelle WHERE mappale ilike '$value%' and foglio ilike '$fg' and (sezione ilike '$sezione' or sezione is null) $filtroComune order by 2";
         if($db->sql_query($sql)){
             $res=$db->sql_fetchrowset();
@@ -207,11 +207,11 @@ switch($field) {
                 "id"=>'',
                 "value"=>'',
                 "label"=>"Si è verificato un errore nell' esecuzione dell'interrogazione $sql;"
-
+                
             );
         break;
     case 'via':
-        $filtroComune = ($_REQUEST["cod_belfiore"])?(" AND comune = '".$_REQUEST["cod_belfiore"]."'"):("");
+	$filtroComune = ($_REQUEST["cod_belfiore"])?(" AND comune = '".$_REQUEST["cod_belfiore"]."'"):("");
         $sql="SELECT DISTINCT nome as valore FROM civici.pe_vie WHERE nome ilike '%$value%' order by 1";
         if($db->sql_query($sql)){
             $res=$db->sql_fetchrowset();
@@ -221,7 +221,7 @@ switch($field) {
                     "value"=>$res[$i]["valore"],
                     "label"=>$res[$i]["valore"]
                 );
-
+                
             }
             $exec=1;
         }
@@ -230,7 +230,7 @@ switch($field) {
                 "id"=>'',
                 "value"=>'',
                 "label"=>"Si è verificato un errore nell' esecuzione dell'interrogazione $sql"
-
+                
             );
         break;
     case 'civico':
@@ -252,7 +252,7 @@ switch($field) {
                 "id"=>'',
                 "value"=>'',
                 "label"=>"Si è verificato un errore nell' esecuzione dell'interrogazione $sql;"
-
+                
             );
         break;
     case "titolo":
@@ -260,36 +260,36 @@ switch($field) {
         $tabella='pe.soggetti';
         break;
     case 'parere':
-        $tabella="pe.pareri";
-        break;
+            $tabella="pe.pareri";
+            break;
     case 'notaio':
-        $tabella="pe.asservimenti";
-        break;
+            $tabella="pe.asservimenti";
+            break;
     case 'destuso1':
     case 'destuso2':
-        $field="destuso";
-        $tabella='pe.e_destuso';
-        break;
+            $field="destuso";
+            $tabella='pe.e_destuso';
+            break;
     case 'motivo':
-        $tabella="pe.sopralluoghi";
-        break;
+            $tabella="pe.sopralluoghi";
+            break;
     case 'motivo_v':
-        $tabella="vigi.sopralluoghi";
-        break;
+            $tabella="vigi.sopralluoghi";
+            break;
     case 'origine':
-        $tabella="vigi.esposti";
-        break;
+            $tabella="vigi.esposti";
+            break;
     case 'intervento':
-        $tabella="pe.e_intervento";
-        break;
+            $tabella="pe.e_intervento";
+            break;	
     case 'nota':
-        $tabella="pe.e_voci_iter";
-        break;
+            $tabella="pe.e_voci_iter";
+            break;			
     case 'sede1':
-        $tabella="ce.commissione";
-        break;
+            $tabella="ce.commissione";
+            break;
     case 'numero-pratica':
-    case "rif_pratica":
+    case "rif_pratica":    
         $sql="SELECT numero as valore, B.nome||' n° '|| coalesce(numero,'') ||  coalesce(' del ' ||to_char(data_prot,'DD/MM/YYYY'),'') as label,B.nome as categoria,coalesce(data_prot,data_presentazione) as data_prot,pratica FROM pe.avvioproc A left join pe.e_tipopratica B on (A.tipo=B.id) WHERE numero ilike '$value%' order by 3,4;";
         if($db->sql_query($sql)){
             $res=$db->sql_fetchrowset();
@@ -299,12 +299,12 @@ switch($field) {
                     "value"=>$res[$i]["valore"],
                     "label"=>$res[$i]["label"],
                     "category"=>$res[$i]["categoria"],
-                    "child"=>Array("riferimento"=>$res[$i]["pratica"])
+					"child"=>Array("riferimento"=>$res[$i]["pratica"])
                 );
 
             }
         }
-        $query=1;
+		$query=1;
         break;
     case "stp-preview":
         $dbh = utils::getDb();
@@ -343,8 +343,8 @@ switch($field) {
 
         $query=1;
         break;
-    case "infrazione":
-    case "esposto":
+	case "infrazione":
+	case "esposto":    
         $sql="SELECT numero as valore, nome||' n° '|| coalesce(numero,'') ||  coalesce(' del ' ||to_char(data_prot,'DD/MM/YYYY'),'') as label,
 		B.nome as categoria,coalesce(data_prot,data_presentazione) as data_prot,pratica FROM vigi.avvioproc A left join vigi.e_tipopratica B on (A.tipo=B.id) WHERE tipologia='$field' AND numero ilike '$value%' order by 3,4;";
         if($db->sql_query($sql)){
@@ -359,9 +359,9 @@ switch($field) {
 
             }
         }
-        $query=1;
+		$query=1;
         break;
-    case "pedilizia":
+	case "pedilizia":
         $sql="SELECT numero as valore, nome||' n° '|| coalesce(numero,'') ||  coalesce(' del ' ||to_char(data_prot,'DD/MM/YYYY'),'') as label,
 		B.nome as categoria,coalesce(data_prot,data_presentazione) as data_prot,pratica FROM pe.avvioproc A left join pe.e_tipopratica B on (A.tipo=B.id) WHERE numero ilike '$value%' order by 3,4;";
         if($db->sql_query($sql)){
@@ -376,7 +376,7 @@ switch($field) {
 
             }
         }
-        $query=1;
+		$query=1;
         break;
 }
 if (!$result && $query==0){
@@ -389,7 +389,7 @@ if (!$result && $query==0){
                 "value"=>$res[$i]["valore"],
                 "label"=>$res[$i]["valore"]
             );
-
+            
         }
     }
     elseif(isset($exec))
@@ -399,8 +399,8 @@ if (!$result && $query==0){
             "id"=>'',
             "value"=>'',
             "label"=>"Si è verificato un errore nell' esecuzione dell'interrogazione;",
-            "query"=>$sql,
-            "field"=>$field
+			"query"=>$sql,
+			"field"=>$field
         );
 }
 header('Content-Type: application/json');
