92d3bc3a0b0a57a05fef9e25e82a2bc2a6ca01af
mamogmx <marco.carbone@gmx.com>
2019-04-08 11:23:06 +0200

    Nascondi campi filtro online

--- pe.ricerca_online.php
+++ pe.ricerca_online.php
@@ -50,11 +50,10 @@ utils::loadCss(Array("default/easyui","icon"));
                 </table>
             </td>
             <td valign="top">
-                
-                <table>
-                    <tr>
-                         <input type="hidden" datatable="pe.avvioproc" id="op_pe-avvioproc-online" class="search text" name="online" value="equal">
-                         <input type="hidden" value="1" id="1_pe-avvioproc-online" name="online" class="text">
+                <input type="hidden" datatable="pe.avvioproc" id="op_pe-avvioproc-online" class="search text" name="online" value="equal">
+                <input type="hidden" value="1" id="1_pe-avvioproc-online" name="online" class="text">
+                <table id="table-filter">
+                    <tr id="tr-assegnata_istruttore">
                         <td valign="middle">
                             <label for="assegnata_istruttore" class="title">Istruttore assegnato</label><br/>
                             <input type="hidden" datatable="pe.vista_assegnate" id="op_pe-vista_assegnate-assegnata_istruttore" class="search text check" name="assegnata_istruttore" value="equal">                           
@@ -64,6 +63,32 @@ utils::loadCss(Array("default/easyui","icon"));
                             <label for="2_pe-vista_assegante-assegnata_istruttore" class="value">SI</label><br/>
                             <input type="radio" value="" id="1_pe-vista_assegante-assegnata_istruttore" name="assegnata_istruttore"  data-plugins="dynamic-search">
                             <label for="3_pe-vista_assegante-assegnata_istruttore" class="value">Tutte</label><br/>
+                        </td>
+                    </tr>
+                    <tr id="tr-sportello">
+                        <td valign="middle">
+                            <label for="sportello" class="title">Sportello di Presentazione</label><br/>
+                            <input type="hidden" datatable="pe.avvioproc" id="op_pe-avvioproc-sportello" class="search text check" name="sportello" value="equal">                           
+                            <input type="radio" value="SUE" id="1_pe-avvioproc-sportello" name="sportello"  data-plugins="dynamic-search">
+                            <label for="1_pe-avvioproc-sportello" class="value">SUE</label><br/>
+                            <input type="radio" value="SUAP" id="2_pe-avvioproc-sportello" name="sportello"  data-plugins="dynamic-search">
+                            <label for="2_pe-avvioproc-sportello" class="value">SUAP</label><br/>
+                            <input type="radio" value="%" id="3_pe-avvioproc-sportello" name="sportello"  data-plugins="dynamic-search">
+                            <label for="3_pe-avvioproc-sportello" class="value">Tutti</label><br/>
+                        </td>
+                    </tr>
+                    <tr id="tr-stato_istruttoria">
+                        <td valign="middle">
+                            <label for="stato_istruttoria" class="title">Sportello di Presentazione</label><br/>
+                            <input type="hidden" datatable="pe.stato_pratica" id="op_pe-stato_pratica-stato_istruttoria" class="search text check" name="stato_istruttoria" value="equal">                           
+                            <input type="radio" value="inizio" id="1_pe-stato_pratica-stato_istruttoria" name="stato_istruttoria"  data-plugins="dynamic-search">
+                            <label for="1_pe-stato_pratica-stato_istruttoria" class="value">SUE</label><br/>
+                            <input type="radio" value="chiusa" id="2_pe-stato_pratica-stato_istruttoria" name="stato_istruttoria"  data-plugins="dynamic-search">
+                            <label for="2_pe-stato_pratica-stato_istruttoria" class="value">SUAP</label><br/>
+                            <input type="radio" value="chiusa_istruttoria" id="3_pe-stato_pratica-stato_istruttoria" name="stato_istruttoria"  data-plugins="dynamic-search">
+                            <label for="3_pe-stato_pratica-stato_istruttoria" class="value">Tutti</label><br/>
+                            <input type="radio" value="%" id="4_pe-stato_pratica-stato_istruttoria" name="stato_istruttoria"  data-plugins="dynamic-search">
+                            <label for="3_pe-stato_pratica-stato_istruttoria" class="value">Tutti</label><br/>
                         </td>
                     </tr>
                 </table>
