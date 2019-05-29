de9bc2d7c9ef124b12947ae1d686148e1b66a5d3
mamogmx <marco.carbone@gmx.com>
2019-05-29 08:38:04 +0200

    Aggiunto dropzone in visione,titolo integrazione

--- pe.integrazioni.php
+++ pe.integrazioni.php
@@ -18,8 +18,8 @@ appUtils::setVisitata($idpratica,basename(__FILE__, '.php'),$_SESSION["USER_ID"]
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <?php
-	utils::loadJS();
-	utils::loadCss();
+    utils::loadCss(Array('dropzone','iter'));
+    utils::loadJS(Array('dropzone'));
 ?>
 </head>
 <body  background="" leftMargin="0" topMargin="0" marginheight="0" marginwidth="0">
--- pe.visione.php
+++ pe.visione.php
@@ -31,8 +31,8 @@ appUtils::setVisitata($idpratica,basename(__FILE__, '.php'),$_SESSION["USER_ID"]
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <?php
-    utils::loadCss();
-    utils::loadJS();
+    utils::loadCss(Array('dropzone','iter'));
+    utils::loadJS(Array('dropzone'));
 ?>
 <script language=javascript>
 function confirmSubmit()
