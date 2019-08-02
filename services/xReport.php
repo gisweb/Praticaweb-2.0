d4290758cc98a96aaa8704fc25c343fa15fffbbe
Marco Carbone <mamo@gw-iol>
2019-08-02 16:37:12 +0200

    A

--- services/xReport.php
+++ services/xReport.php
@@ -101,7 +101,7 @@ LEFT JOIN ragioneria.flussi G ON(A.codice_univoco=G.iuv)
 SELECT 
     numero as "Numero",protocollo as "Protocollo",data_prot as "Data Prot.",format('%s %s',tipo,categoria) as "Tipo Pratica",
     importo as "Importo",data_pagamento as "Data Pagam.",tipo_pagamento as "Tipo Pagam.",capitolo as "Capitolo",causale as "Causale", 
-    codice_univoco as "Codice Pagam.",flusso as "Flusso",anagrafica as "Anagrafica",identificativofiscale as "C.F./P.IVA"    
+    format('IUV : %s',codice_univoco::text) as "Codice Pagam.",flusso as "Flusso",anagrafica as "Anagrafica",identificativofiscale as "C.F./P.IVA"    
 FROM search_pagamenti 
 WHERE 
     id in ($_REQUEST[elenco]);
