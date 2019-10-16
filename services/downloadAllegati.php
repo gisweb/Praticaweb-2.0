e7f419edfd4d26a2e3f804d51972292f51aacb8c
Marco Carbone <mamo@gw-iol>
2019-10-16 12:47:55 +0200

    Some fix

--- services/downloadAllegati.php
+++ services/downloadAllegati.php
@@ -1,5 +1,4 @@
 <?php
-error_reporting(E_ALL);
 require_once "../login.php";
 $dbh = utils::getDB();
 $pratica = $_REQUEST["pratica"];
@@ -7,11 +6,11 @@ $pr = new pratica($pratica);
 
 $allegatiDir = $pr->allegati.$fName;
 if ($_REQUEST["stato_allegato"] && $_REQUEST["stato_allegato"]!="all"){
-    $sql = "SELECT id,nome_file,prot_allegato,data_prot_allegato FROM pe.file_allegati WHERE pratica = ? AND stato_allegato = ?;";
+    $sql = "SELECT id,nome_file,prot_allegato,data_prot_allegato FROM pe.file_allegati WHERE pratica = ? AND stato_allegato = ? and coalesce(nome_file,'')<>'';";
     $data = Array($pratica,$_REQUEST["stato_allegato"]);
 }
 else{
-    $sql = "SELECT id,nome_file,prot_allegato,data_prot_allegato FROM pe.file_allegati WHERE pratica = ?;";
+    $sql = "SELECT id,nome_file,prot_allegato,data_prot_allegato FROM pe.file_allegati WHERE pratica = ? and coalesce(nome_file,'')<>'';";
     $data = Array($pratica);
 }
 $stmt = $dbh->prepare($sql);
@@ -22,18 +21,23 @@ if($stmt->execute($data)){
     $zip = new ZipArchive;
     chdir(DATA_DIR."tmp");
     $archive_file_name = $pratica.".zip";
-    
+//error_reporting(E_ALL);    
     if ($zip->open($archive_file_name,  ZipArchive::CREATE)) {
         for($i=0;$i<count($res);$i++){
             $r = $res[$i];
-			$fileName = sprintf("%s%s",$allegatiDir,$r["nome_file"]);
-//			utils::debug(DEBUG_DIR."TEST.debug",$fileName,'a+');
+	    $fileName = sprintf("%s%s",$allegatiDir,$r["nome_file"]);
+//	    utils::debug(DEBUG_DIR."TEST.debug",$fileName,'a+');
+            if (file_exists($fileName)){
 	                if(! $zip->addFile($fileName, $r["nome_file"])){
-			    utils::debug(DEBUG_DIR."TEST.debug",$fileName,'a+');
+			    utils::debug(DEBUG_DIR."DOWNLOAD.debug",$fileName,'a+');
                         }
-			//else{
-			//    utils::debug(DEBUG_DIR."TEST.debug",$fileName,'a+');
-			//}
+			else{
+			    utils::debug(DEBUG_DIR."DOWNLOAD-ERROR.debug",$fileName,'a+');
+			}
+            }
+            else{
+                utils::debug(DEBUG_DIR."DOWNLOAD-ERROR-NotFound.debug",$fileName,'a+');
+            }
             
             
         }
